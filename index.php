79ece6cf5b170f94381b8ea0899caba030c27be7
Gordon Murray <gordon@gordonmurray.com>
2019-05-26 15:08:46 +0100

    Including an example environment variable

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
 
-echo "Hell World From Kubernetes";
+echo "Hello World From Kubernetes";
+
+?>
+
+<hr >
+
+<?php
+
+// Example use of getenv()
+$database_user = getenv('database_user');
+
+echo "Database username: " . $database_user;
 
