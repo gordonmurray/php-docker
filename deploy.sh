77cc8f653d516c7e92e811eca19da8261754f71f
Gordon Murray <gordon@gordonmurray.com>
2019-05-26 18:30:50 +0100

    working env variable

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
  #!/usr/bin/env bash
 
-TAG="0.05"
-YOUR_DOCKERHUB_USERNAME=XXXXX
+TAG="0.5"
+YOUR_DOCKERHUB_USERNAME=xxxxx
+DATABASE_USER=`echo "testing" | base64`
 
-docker login --username=$YOUR_DOCKERHUB_USERNAME --password="XXXXX" 
+docker login --username=$YOUR_DOCKERHUB_USERNAME --password="xxxxx" 
 
 docker build . -t php-hello-world
 
@@ -11,7 +12,7 @@ docker tag php-hello-world $YOUR_DOCKERHUB_USERNAME/php-hello-world:$TAG
 
 docker push $YOUR_DOCKERHUB_USERNAME/php-hello-world:$TAG
 
-helm upgrade php-hello-world --install --wait ./php-hello-world --set service.type=NodePort --namespace default --set image.tag="$TAG" --set database_user="fred"
+helm upgrade php-hello-world --install --wait ./php-hello-world --set service.type=NodePort --namespace default --set image.tag="$TAG" --set database_user="$DATABASE_USER"
 
 export NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services php-hello-world)
 export NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
